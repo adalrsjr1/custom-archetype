c941129721c58a5c8202c300e83e855997ca1803
adalrsjr1 <adalrsjr1@gmail.com>
2016-09-10 17:47:09 -0300

    first commit

---
+++ target/classes/archetype-resources/src/main/groovy/App.groovy
@@ -0,0 +1,16 @@
+package com.github.adalrsjr1.${artifactId}
+
+import groovy.util.logging.Slf4j
+/**
+ * Hello world!
+ *
+ */
+@Slf4j
+public class App 
+{
+    public static void main( String[] args )
+    {
+        println "Hello World!"
+		log.info "Hello World!"
+    }
+}
